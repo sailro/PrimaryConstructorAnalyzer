940f2055f6c0627a5aff40bef73eca7c41d63831
Sebastien Lebreton <sebastien@lebreton.fr>
2023-11-24 11:51:39 +0100

    Remove unused dependency

--- PrimaryConstructorAnalyzer.Test/PrimaryConstructorAnalyzerUnitTests.cs
+++ PrimaryConstructorAnalyzer.Test/PrimaryConstructorAnalyzerUnitTests.cs
@@ -1,59 +1,58 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VerifyCS = PrimaryConstructorAnalyzer.Test.CSharpAnalyzerVerifier<PrimaryConstructorAnalyzer.PrimaryConstructorParameterMutationAnalyzer>;
 
-namespace PrimaryConstructorAnalyzer.Test
+namespace PrimaryConstructorAnalyzer.Test;
+
+[TestClass]
+public class PrimaryConstructorAnalyzerUnitTest
 {
-	[TestClass]
-	public class PrimaryConstructorAnalyzerUnitTest
+	[TestMethod]
+	public async Task TestRegularCtorAsync()
 	{
-		[TestMethod]
-		public async Task TestRegularCtorAsync()
-		{
-			const string test = """
-			                    class Foo
-			                    {
-			                        public Foo(int i) {
-			                            i = 0;
-			                        }
-			                    }
-			                    """;
+		const string test = """
+		                    class Foo
+		                    {
+		                        public Foo(int i) {
+		                            i = 0;
+		                        }
+		                    }
+		                    """;
 
-			await VerifyCS.VerifyAnalyzerAsync(test);
-		}
+		await VerifyCS.VerifyAnalyzerAsync(test);
+	}
 
-		[TestMethod]
-		public async Task TestPrimaryCtorParameterReadAsync()
-		{
-			const string test = """
-			                    class Foo(int i)
-			                    {
-			                        void Bar() {
-			                            int j = i + 12;
-			                        }
-			                    }
-			                    """;
+	[TestMethod]
+	public async Task TestPrimaryCtorParameterReadAsync()
+	{
+		const string test = """
+		                    class Foo(int i)
+		                    {
+		                        void Bar() {
+		                            int j = i + 12;
+		                        }
+		                    }
+		                    """;
 
-			await VerifyCS.VerifyAnalyzerAsync(test);
-		}
+		await VerifyCS.VerifyAnalyzerAsync(test);
+	}
 
-		[TestMethod]
-		public async Task TestPrimaryCtorParameterMutateAsync()
-		{
-			const string test = """
-			                    class Foo(int i)
-			                    {
-			                        void Bar() {
-			                            i = 42;
-			                        }
-			                    }
-			                    """;
+	[TestMethod]
+	public async Task TestPrimaryCtorParameterMutateAsync()
+	{
+		const string test = """
+		                    class Foo(int i, string bar)
+		                    {
+		                        void Bar() {
+		                            i = 42;
+		                        }
+		                    }
+		                    """;
 
-			var diagnostic = VerifyCS
-				.Diagnostic(PrimaryConstructorParameterMutationAnalyzer.DiagnosticId)
-				.WithLocation(4, 9)
-				.WithArguments("i");
+		var diagnostic = VerifyCS
+			.Diagnostic(PrimaryConstructorParameterMutationAnalyzer.DiagnosticId)
+			.WithLocation(4, 9)
+			.WithArguments("i");
 
-			await VerifyCS.VerifyAnalyzerAsync(test, diagnostic);
-		}
+		await VerifyCS.VerifyAnalyzerAsync(test, diagnostic);
 	}
 }
